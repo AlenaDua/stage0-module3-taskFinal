341f3667b1e2a0751e8b42ded83b36b3a0100ce2
Alena Dubadzelava <dubodelova.minsk@gmail.com>
2023-02-02 16:45:18 +0100

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int number1 = number / 100;
+        int number2 = (number % 100) / 10;
+        int number3 = number % 10;
+
+        System.out.printf("%d%d%d\n", number3, number2, number1);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double celsius = temperatureCelsius;
+        double fahrenheit;
+        fahrenheit = (celsius * 1.8) + 32;
+        System.out.printf("%.1f\n",fahrenheit);
     }
 }
